3b81a8e4b0043573727d2190269735c8997d1d7c
Vladimir Sizov <sizov.v.f@gmail.com>
2017-01-12 10:23:31 +0000

    redux connect VS component passig properties

--- source/components/counter/Bar.js
+++ source/components/counter/Bar.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Toggler from './Toggler';
 
+//TODO: big question: what is reusable COMPONENT??? is it view&reducer? is it redux container?
+
 export default (props) => {
 //TODO: This style VS redux connect: redux connect has access to the whole state
 //TODO: which is really bad for encapsulation reasons.
 //TODO: in this way we can pass downstream nested properties wihtout having to know
 //TODO: where it is located in the WHOLE state
 
- return <div style={{padding: '5px', margin: '0 auto', border: 'solid'}}>
-    <Toggler />
+  return <div style={{padding: '5px', margin: '0 auto', border: 'solid'}}>
+    <Toggler
+      enabled={props.enabled}
+      toggle={() => props.toggle()}/>
   </div>
 
 };
--- source/containers/BarContainer.js
+++ source/containers/BarContainer.js
@@ -10,8 +10,17 @@ const getTiggleBarAction = () => ({
 //TODO: which is really bad for encapsulation reasons.
 //TODO: research doing it just by passing required part of the state form store?
 
-//fixme: connect passes all state, not small slice!!
+//TODO: but please keep in mind that container doe more:
+//TODO: 1) has ref to .dispatch method to dispatch actions
+
+//fixme: !!!! connect passes all state, not small slice!!
 //todo: probably the selector should be used here
+
+//FIXME: idea: 'connect' might receive just 'mapDispatchToProps'
+//FIXME: -> so then you can use connect to get a ref to .dispatch but
+//FIXME: pass state as a properties to omit componentns
+//FIXME: getting access to the WHOLE state, give them just a slice
+//FIXME: connect(){MyComponent} will get access to .dispatch in MyCOmponent as a prop
 const mapStateToProps = (state) => {
   return {
     enabled: state.bar.enabled
