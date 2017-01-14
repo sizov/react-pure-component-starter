d4cc1820282be06b7bd96c416b991c16b31c18b6
Vladimir Sizov <sizov.v.f@gmail.com>
2017-01-14 11:49:41 +0000

    using empty connect() for pure component encapsulation

--- source/components/containers/bar/defaultState.js
+++ source/components/containers/bar/defaultState.js
@@ -1,3 +1,3 @@
 export default {
-  enabled: false
+  enabled: true
 };
--- source/store/reducers/rootReducer.js
+++ source/store/reducers/rootReducer.js
@@ -1,10 +1,10 @@
 import barReducer from '../../components/containers/bar/reducer';
 import fooReducer from '../../components/containers/foo/reducer';
-import counterReducer from '../../components/containers/app-counter/reducer';
+import appCounterReducer from '../../components/containers/app-counter/reducer';
 
 export default (state = {}, action) => {
   return {
-    counter: counterReducer(state.counter, action),
+    counter: appCounterReducer(state.counter, action),
     foo: fooReducer(state.foo, action),
     bar: barReducer(state.bar, action)
   };
